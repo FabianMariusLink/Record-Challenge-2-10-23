df30c61c7d04dc92452fea47da84c7945af2487b
iskrenr <iskrenr005@gmail.com>
2023-10-02 10:45:47 +0200

    Task 2: Create 3 instances of Animal and check the methods toString() and equals()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Animal tiger = new Animal("1", "Tiger1", "African tiger", 12);
+        Animal lion  = new Animal("2", "Lion1", "South African lion", 7);
+        Animal elephant = new Animal("3", "Elephant1", "Indian elephant", 15);
 
+        System.out.println(tiger);
+        System.out.println(lion);
+        System.out.println(elephant);
+
+        System.out.println(lion.equals(elephant));
     }
 }
